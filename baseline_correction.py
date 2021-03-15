3288d961e9cfa6cb3e9d34346dc516dd88ef6862
Alxmrphi <murphyalex@gmail.com>
2021-03-15 11:44:13 +0000

    Update baseline_correction.py
    Added apply function

--- baseline_correction.py
+++ baseline_correction.py
@@ -1,7 +1,7 @@
 # Pre-sentence baseline correction
 # - Two main functions
 # -- 1) generate_baseline_dict
-# -- 2) apply_baseline_dict
+# -- 2) apply_baseline
 
 # (1) Explanation:
 # - A folder exists with all the mne.Epochs files for each session
@@ -14,7 +14,7 @@
 # - Store / save this value to be applied in the apply_baseline step.
 
 # (2) Explanation:
-
+# - Using the generated baseline dict
 
 
 import numpy as np
@@ -29,7 +29,7 @@ import random
 epoch_dir = './extended_epochs_minICA'
 write_filename = 'baseline_dict_minICA.pkl'
 
-def generate_baseline_dict(epoch_dir, write_filename):
+def generate_baseline_dict(epoch_dir, baseline_filename) -> None:
 
     epoch_folder = Path(epoch_dir)
     epoch_files = list(epoch_folder.iterdir())
@@ -64,7 +64,51 @@ def generate_baseline_dict(epoch_dir, write_filename):
         baseline_dict[sent_id] = baseline_avg
     
     # Write out results
-    pickle.dump(baseline_dict, open(write_filename, 'wb'))
+    pickle.dump(baseline_dict, open(baseline_filename, 'wb'))
 
 
-def apply_baseline_dict()
+def apply_baseline(epoch_dir, baseline_filename) -> None:
+
+    baseline = pickle.load(open(baseline_filename, 'rb'))
+
+    epoch_folder = Path(epoch_dir)
+    epoch_files = list(epoch_folder.iterdir())
+    np.random.shuffle(epoch_files)
+
+    for epoch_file in epoch_files:
+
+        #if "13-20" not in str(epoch_file):
+        #    continue
+
+        tmp_epochs = mne.read_epochs(str(epoch_file), preload=True)
+        tmp_sent_ids = tmp_epochs.metadata.sent_ident
+        tmp_set_sent_ids = list(set(tmp_sent_ids))
+
+        idx_checker = []
+        error_counter = []
+
+        for tmp_sent_id in tmp_set_sent_ids:
+            # These are the elements of the corresponding sentence
+            idx = tmp_sent_ids[tmp_sent_ids == tmp_sent_id].index
+            # For indexing into base data
+            start, end = idx[0], idx[-1]+1
+            # Track how many idx are processed
+            idx_checker.extend(idx)
+            # Pull out baseline correction for this sent_id
+            #assert tmp_sent_id in baseline.keys(), f"Not found baseline for {tmp_sent_id}"
+            if tmp_sent_id not in baseline.keys():
+                error_counter.append([tmp_sent_id])
+                continue
+            else:        
+                tmp_baseline = baseline[tmp_sent_id]
+
+            # Loop through EEG channels and baseline correct in-place
+            for i in range(len(tmp_epochs.ch_names)):
+                x = tmp_epochs._data
+                assert id(x) == id(tmp_epochs._data), "id mismatch"
+                x[start:end,i,:] = x[start:end,i,:] - tmp_baseline[i]
+
+            # assert len(idx_checker) == len(tmp_epochs), "Not all events baseline-corrected"
+
+        new_filename = str(epoch_file)[:-10] + str('bc_epo.fif')
+        tmp_epochs.save(new_filename, overwrite=True)
